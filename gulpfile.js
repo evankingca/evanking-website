71c2dcd2471173de1d1a553f4993e6e168bfa702
evan <57847413+evankingca@users.noreply.github.com>
2025-02-16 01:13:38 -0500

    Update Gulpfile to fix broken fonts

--- gulpfile.js
+++ gulpfile.js
@@ -29,13 +29,20 @@ function buildStyles() {
 
 // MOVE FILES IN ROOT DIR TO DIST
 const moveFilesSrcPath = [
-    "fonts/*.css",
-    "fonts/*.woff2",
     // "img/*",
     "js/*"
 ];
 function moveFiles() {
   return gulp.src(moveFilesSrcPath, { base: "." }).pipe(gulp.dest("dist"));
+}
+
+// FONTS REQUIRE ENCODING DISABLED TO WORK:
+const moveFontsSrcPath = [
+    "fonts/*.css",
+    "fonts/*.woff2",
+];
+function moveFonts() {
+  return gulp.src(moveFontsSrcPath, { base: ".", encoding: false }).pipe(gulp.dest("dist"));
 }
 
 // IT'S WATCHING YOU
@@ -45,6 +52,6 @@ function watchTasks() {
 }
 
 // EXPORT FOR WATCHING
-exports.default = gulp.series(includeFiles, buildStyles, moveFiles, watchTasks);
+exports.default = gulp.series(includeFiles, buildStyles, moveFiles, moveFonts, watchTasks);
 
-exports.bld = gulp.series(includeFiles, buildStyles, moveFiles);
+exports.bld = gulp.series(includeFiles, buildStyles, moveFiles, moveFonts);
